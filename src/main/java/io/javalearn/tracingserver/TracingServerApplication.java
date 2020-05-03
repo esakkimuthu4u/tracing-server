b60d72f2202daf59cd8a45e259ba46fc36a4d957
esakkimuthuvelraj <ev@findroids.com>
2020-05-03 20:12:13 +0530

    first init commit

---
+++ src/main/java/io/javalearn/tracingserver/TracingServerApplication.java
@@ -0,0 +1,13 @@
+package io.javalearn.tracingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracingServerApplication.class, args);
+	}
+
+}
